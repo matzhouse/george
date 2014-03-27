f680aaa05788862f5eb64e0ccb643640de3f7bcd
Mat Evans <mat@coull.com>
2014-03-27 14:57:13 +0000

    examples for testing

--- george/agent.go
+++ george/agent.go
@@ -2,10 +2,29 @@ package george
 
 import (
 	"fmt"
+	"github.com/matzhouse/go-metrics"
+	"github.com/matzhouse/go-metrics/http"
+	"time"
 )
 
 func Start() {
 
-	fmt.Println("Waking george up!")
+	fmt.Println("Waking george up..")
+
+	registry := metrics.NewRegistry()
+
+	c := metrics.NewCounter()
+	registry.Register("test", c)
+	c.Inc(1)
+
+	go http.Start(registry)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			time.Sleep(10 * time.Second)
+		}()
+	}
+
+	time.Sleep(60 * time.Second)
 
 }
